b007e626be42c8f7e83f2a50e87b703b0e56e588
sethvincent <sethvincent@gmail.com>
2015-11-08 15:32:16 -0800

    cleanup

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 var app = require('./server/index')()
-
-var socketio = require('socket.io')(app);
-
-var fs = require('fs');
-
+var socketio = require('socket.io')(app)
 var server = {}
 var realtime = require('./server/lib/realtime-editing')(socketio, server)
-
 app.listen()
