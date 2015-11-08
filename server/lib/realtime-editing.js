003d341300248c82992b24a3ee3fca6c7d5c9a36
Luke Swart <lukeswart@gmail.com>
2015-11-07 23:34:35 -0800

    Add realtime-editing (mostly from Flatsheet)

---
+++ server/lib/realtime-editing.js
@@ -0,0 +1,58 @@
+var extend = require('extend');
+
+module.exports = function (io, server) {
+  var rooms = {};
+
+  io.on('connection', function (socket) {
+    console.log("io connected")
+    socket.on('room', function (room) {
+      console.log("get this party started");
+      console.log("room event received:", room)
+      // socket.emit('winner', { hello: 'world'})
+    //   socket.join(room);
+
+    //   if (!rooms[room]) rooms[room] = { users: {} };
+
+    //   socket.on('user', function (user) {
+    //     if (!rooms[room].users[socket.id]) {
+    //       rooms[room].users[socket.id] = user;
+    //     }
+    //     io.to(room).emit('update-users', rooms[room].users);
+    //   });
+
+    //   server.sheets.get(room, function (err, sheet) {
+    //     socket.on('change', function (change, rows, sort) {
+    //       sheet.rows = rows;
+
+    //       server.sheets.update(room, sheet, function (err, sheet) {
+    //         if (err) console.error(err);
+    //         socket.broadcast.to(room).emit('change', change, rows, sort);
+    //       });
+    //     });
+
+    //     socket.on('sheet-details', function (change) {
+    //       sheet = extend(sheet, change);
+          
+    //       server.sheets.update(sheet, function (err) {
+    //         if (err) console.error(err);
+    //         socket.broadcast.to(room).emit('sheet-details', change);
+    //       });
+    //     });
+
+    //     socket.on('cell-focus', function (cell, color) {
+    //       io.to(room).emit('cell-focus', cell, color);
+    //     });
+
+    //     socket.on('cell-blur', function (cell) {
+    //       io.to(room).emit('cell-blur', cell);
+    //     });
+    //   });
+
+    //   socket.on('disconnect', function () {
+    //     io.to(room).emit('cell-blur');
+    //     delete rooms[room].users[socket.id];
+    //     io.to(room).emit('update-users', rooms[room].users);
+    //   });
+    });
+  })
+}
